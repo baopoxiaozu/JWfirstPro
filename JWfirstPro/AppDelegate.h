3fde6792d10ab1cca52c9127249a48c4736267c1
apple <apple@jinwei.local>
2016-12-13 11:29:56 +0800

    first

---
+++ JWfirstPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWfirstPro
+//
+//  Created by apple on 16/12/13.
+//  Copyright © 2016年 fenger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
