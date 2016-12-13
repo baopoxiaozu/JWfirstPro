395e2d75e83238e842b714d81592daebcbf1f976
smileKH <2448471861@qq.com>
2016-12-13 11:56:59 +0800

    ssssss

--- JWfirstPro/ViewController.m
+++ JWfirstPro/ViewController.m
@@ -20,6 +20,19 @@
     label.backgroundColor = [UIColor redColor];
     label.text = @"哈哈";
     [self.view addSubview:label];
+    
+    UILabel *label1 = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
+    label1.backgroundColor = [UIColor redColor];
+    label1.text = @"哈哈";
+    [self.view addSubview:label1];
+    
+    
+    UILabel *label2 = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
+    label2.backgroundColor = [UIColor redColor];
+    label2.text = @"哈哈";
+    [self.view addSubview:label2];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
